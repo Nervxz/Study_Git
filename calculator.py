31a2645d33b826aa47d763c14c06e62d0449b51b
Thai Nguyen Khang <127386953+Nervxz@users.noreply.github.com>
2026-01-14 16:27:57 +0700

    feature_calculator

---
+++ calculator.py
@@ -0,0 +1,23 @@
+"""
+Simple Calculator
+"""
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b != 0:
+        return a / b
+    return "Cannot divide by zero"
+
+if __name__ == "__main__":
+    print("5 + 3 =", add(5, 3))
+    print("10 - 4 =", subtract(10, 4))
+    print("6 * 7 =", multiply(6, 7))
+    print("20 / 4 =", divide(20, 4))
